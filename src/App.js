ec4044a801ac267931a8becca09ec492c8170dc3
Kai Lin Zhang <kaizen.agility@gmail.com>
2017-11-09 23:41:42 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import recipes from './data/recipes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="flex-container">
+        Start writing your app here!
+      </div>
+    );
+  }
+}
+
+export default App;
